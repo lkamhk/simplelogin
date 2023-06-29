4e906b8fda3e567f49e10ed822edfa8ad50533db
lkamhk <lkhskyhk@hotmail.com>
2023-06-29 20:19:07 +0800

    debug

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import { Inter } from 'next/font/google'
-import { useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
 import { NextPageContext } from "next"
 import { getSession, useSession } from "next-auth/react"
@@ -8,22 +8,22 @@ import useCurrentUser from '../hooks/useCurrentUser'
 const inter = Inter({ subsets: ['latin'] });
 
 
-export async function getServerSideProps(context: NextPageContext){
+export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
 
 
-  if(!session){
+  if (!session) {
     return {
-      redirect:{
+      redirect: {
         destination: '/login',
         permanent: false,
-      
+
       }
     }
   }
 
   return {
-    props:{session}
+    props: { session }
   }
 }
 
@@ -31,36 +31,36 @@ export async function getServerSideProps(context: NextPageContext){
 export default function Home() {
 
 
-   const router = useRouter();
-const {data: session} = useSession();
-const [eMail,seteMail] = useState(null);
-   
-
- useEffect(() => {
-
-if(session)
- seteMail(session.user.email)
+  const router = useRouter();
+  const { data: session } = useSession();
+  const [eMail, seteMail] = useState(null);
 
 
- },[session])
+  useEffect(() => {
+
+    if (session)
+      seteMail(session.user.email || null)
+
+
+  }, [session])
 
 
   return (
-  // <Context.Provider value={userName}>
-  
+    // <Context.Provider value={userName}>
+
     <main
       className={`flex h-[100vh] flex-col items-center justify-content-center ${inter.className}`}
     >
-  <NavBar/>
-    <div className='flex w-[500px] flex-col mt-[100px] border p-[100px] items-center justify-center'>
+      <NavBar />
+      <div className='flex w-[500px] flex-col mt-[100px] border p-[100px] items-center justify-center'>
 
-    <h5>Logged in as: {eMail?eMail:'Loading...'}</h5>
-    <p>
-    {}
-    </p>
-    </div>
- 
+        <h5>Logged in as: {eMail ? eMail : 'Loading...'}</h5>
+        <p>
+          { }
+        </p>
+      </div>
+
     </main>
-  // </Context.Provider>
+    // </Context.Provider>
   )
 }
